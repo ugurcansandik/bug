17ea168a32f5d88574df8583815ef69e20f4508e
Uğurcan SANDIK <44526848+ugurcansandik@users.noreply.github.com>
2021-05-06 04:02:43 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+	    String choice = "";
+	    Scanner scanner = new Scanner(System.in);
+	    System.out.println("Please create a bug.(e.g, create EAST 4 7)");
+	    scanner.next();
+	    Bug bug = new Bug(1, 1, scanner.next(), scanner.nextInt(), scanner.nextInt());
+		while(true)
+		{
+		    choice = scanner.next();
+		    switch(choice)
+		    {
+	            case "move":
+                    bug.move();
+	                break;
+                case "jump":
+                    bug.jump(scanner.nextInt(), scanner.nextInt());
+	                break;
+                case "turnRight":
+                    bug.turnRight();
+                    break;
+                case "turnLeft":
+                    bug.turnLeft();
+                    break;
+                case "turnBack":
+                    bug.turnBack();
+                    break;
+		    }
+		}
+	}
+}
